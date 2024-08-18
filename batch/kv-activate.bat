55b055877f9e6ed4cfb9511b4929041a0b460ddb
Ken Venner <ken@vennerllc.com>
2024-08-18 09:43:28 -0700

    deal with fact that different machines have users in different locatoins

--- batch/kv-activate.bat
+++ batch/kv-activate.bat
@@ -1,10 +1,73 @@
 @echo off
+rem build out the python version environment for venv
+rem
+rem
+rem drop box is different so create unique environment
+SET VUNIQUE=lt
+rem
+rem set directory
+SET VENVDIR=venv
+rem
+rem default is 3.11
+if "%1%" == "" (
+   SET VERSION=11
+) ELSE (
+   echo here
+   SET VERSION=%1%
+)
+rem
+rem pick the version of python
+if "%VERSION%" == "7" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python37\python.exe
+SET VENVDIR=venv07
+)
+if "%VERSION%" == "9" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe
+SET VENVDIR=venv09
+)
+if "%VERSION%" == "10" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe
+SET VENVDIR=venv10
+)
+if "%VERSION%" == "11" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python311\python.exe
+SET VENVDIR=venv11
+)
+if "%VERSION%" == "12" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python312\python.exe
+SET VENVDIR=venv12
+)
+if "%VERSION%" == "13" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python313\python.exe
+SET VENVDIR=venv13
+)
+if "%VERSION%" == "14" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python314\python.exe
+SET VENVDIR=venv14
+)
+if "%VERSION%" == "15" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python315\python.exe
+SET VENVDIR=venv15
+)
+rem
+rem
+rem Make the directory unique
+SET VENVDIR=%VENVDIR%%VUNIQUE%
+rem
+rem
 rem turn on local environment for python if machine supports it
 rem
-if "%computername%" == "KVENNER-X1" (
-   echo Python activate running venv\scripts\activate
-   @echo on
-   call venv\scripts\activate
-) ELSE (
-   echo This computer not setup for venv
-)
+if "%computername%" == "KVENNER-X1" goto validmachine
+if "%computername%" == "KVENNER-LENOVO1" goto validmachine
+if "%computername%" == "DT-WIN11" goto validmachine
+
+echo This computer not setup for venv
+goto end
+
+:validmachine
+echo Python activate running %VENVDIR%\scripts\activate
+call %VENVDIR%\scripts\activate
+echo|set /p="Running python version: "
+python -V
+
+:end
--- batch/kv-venv.bat
+++ batch/kv-venv.bat
@@ -1,37 +1,67 @@
 @echo off
 rem build out the python version environment
 rem
-rem default is 3.7
+rem drop box is different so create unique environment
+SET VUNIQUE=lt
+rem
+rem set directory
+SET VENVDIR=venv
+rem
+rem default is 3.11
 if "%1%" == "" (
-   SET VERSION=7
+   SET VERSION=11
 ) ELSE (
    echo here
    SET VERSION=%1%
 )
 rem
-rem
-if "%VERSION%" == "6" (
-SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python36\python.exe
-)
+rem pick the versoin of python
 if "%VERSION%" == "7" (
 SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python37\python.exe
+SET VENVDIR=venv07
 )
 if "%VERSION%" == "9" (
 SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe
+SET VENVDIR=venv09
 )
 if "%VERSION%" == "10" (
 SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python310\python.exe
+SET VENVDIR=venv10
+)
+if "%VERSION%" == "11" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python311\python.exe
+SET VENVDIR=venv11
+)
+if "%VERSION%" == "12" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python312\python.exe
+SET VENVDIR=venv12
+)
+if "%VERSION%" == "13" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python313\python.exe
+SET VENVDIR=venv13
+)
+if "%VERSION%" == "14" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python314\python.exe
+SET VENVDIR=venv14
+)
+if "%VERSION%" == "15" (
+SET PGM=%USERPROFILE%\AppData\Local\Programs\Python\Python315\python.exe
+SET VENVDIR=venv15
 )
 rem
+rem Make the directory unique
+SET VENVDIR=%VENVDIR%%VUNIQUE%
+rem
+rem
 @echo off
-if exist venv (
-echo VENV already exists - no action required
+if exist %VENVDIR% (
+echo %VENVDIR% already exists - no action required
 goto exit
 )
 @echo on
-if not exist venv (
-%PGM% -m venv venv
-call venv\scripts\activate
+if not exist %VENVDIR% (
+%PGM% -m venv %VENVDIR%
+call %VENVDIR%\scripts\activate
 python -m pip install --upgrade pip
 pip install wheel
 )
